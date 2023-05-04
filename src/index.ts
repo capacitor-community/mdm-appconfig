8c8f4da4f4bb25fdef38d47971b4460deb6dc74a
dtarnawsky <damian@ionic.io>
2023-05-04 06:12:02 -0700

    feat: Capacitor 5 support

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { MDMAppConfigPlugin } from './definitions';
 
-const AppConfig = registerPlugin<MDMAppConfigPlugin>(
-  'MDMAppConfig',
-  {},
-);
+const AppConfig = registerPlugin<MDMAppConfigPlugin>('MDMAppConfig', {});
 
 export * from './definitions';
 export { AppConfig };
